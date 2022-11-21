8ef6e6ebda065838fdb86db10d58119b61ea367d
Lasseignejk <jaye914@hotmail.com>
2022-11-20 21:27:54 -0500

    Updated dictionaryExercises.py
    Changed my answer to challenge 5 so the function letter_histogram() was called inside another function, histogram_tally(). Also added notes for the 'return' keyword.

--- week1/day5/dictionaryExercises.py
+++ week1/day5/dictionaryExercises.py
@@ -42,18 +42,19 @@
 # # Write a letter_histogram program that asks the user for input, and prints a dictionary containing the tally of how many times each letter in the alphabet was used in the word. For example:
 # # $ python letter_histogram.py Please enter a word: banana {'a': 3, 'b': 1, 'n': 2}
 # # In this exercise, are you using dynamic keys or fixed keys?
+word = input("Please enter a word with at least three letters: ")
 
-# def letter_histogram():
-#     letterDictionary = {}
-#     word = input("Please enter a word: ")
-#     dictionaryKeys = letterDictionary.keys()
-#     for letter in word:
-#         if letter not in dictionaryKeys:
-#             letterDictionary[letter] = 1
-#         elif letter in dictionaryKeys:
-#             letterDictionary[letter] += 1
 
-#     print(letterDictionary)
+def letter_histogram():
+    letterDictionary = {}
+    dictionaryKeys = letterDictionary.keys()
+    for letter in word:
+        if letter not in dictionaryKeys:
+            letterDictionary[letter] = 1
+        elif letter in dictionaryKeys:
+            letterDictionary[letter] += 1
+# The return keyword prints the result to the terminal, but also allows it to be stored as the result of a function. If you don't use return and just use print instead, the result will be printed but that's it. You can't use it for something else later.
+    return letterDictionary
 
 
 # letter_histogram()
@@ -83,22 +84,16 @@
 # # Given a histogram tally (one returned from either letter_histogram or word_summary), print the top 3 words or letters.
 
 def histogram_tally():
+    toSort = letter_histogram()
     # This is a sort key, which has nothing to do with a dictionary key. This goes into the 'key' part of sorted(), which takes a callback function. A callback function is a function that's passed as an argument to another function.
+
     def get_value(item):
         return item[1]
-    letterDictionary = {}
-    word = input("Please enter a word with at least 3 letters: ")
-    dictionaryKeys = letterDictionary.keys()
     while len(word) >= 3:
-        for letter in word:
-            if letter not in dictionaryKeys:
-                letterDictionary[letter] = 1
-            elif letter in dictionaryKeys:
-                letterDictionary[letter] += 1
         # So on the next line of code, we're sorting letterDictionary by the values. To do that, we're first using items() to retrieve they key/value pairs in tuples. Once we have those, we pass get_value in the key section. The 'items' are the tuples, which are indexed, and we access the values by using index 1. Those values are then sorted highest to lowest because reverse=True.
         # If we don't have a key in the below, or the reverse=True, they're sorted alphabetically based on the key only.
         # You can also use lambda in the key instead of get_value, but I don't understand that yet.
-        tally = sorted(letterDictionary.items(), key=get_value, reverse=True)
+        tally = sorted(toSort.items(), key=get_value, reverse=True)
         print(
             f"The top three letters in the word '{word}' are: \n {tally[0]}\n {tally[1]} \n {tally[2]}")
         return
