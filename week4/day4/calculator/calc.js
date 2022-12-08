67efe9017c91e18c91cc7eff0f4a0859910060fa
Lasseignejk <jaye914@hotmail.com>
2022-12-08 18:30:06 -0500

    updates to calculator
    Have a screen, can save user digit inputs to an array

--- week4/day4/calculator/calc.js
+++ week4/day4/calculator/calc.js
@@ -1,22 +1,54 @@
 // select, create/modify, append
 
-const calcDiv = document.querySelector(".mainContainer");
+
 
 // select
+const calcDiv = document.querySelector(".mainContainer");
+const screen = document.querySelector(".numScreen")
 // this returns an object/dictionary
 const calcButton = document.querySelectorAll(".num")
 const calcOperator = document.querySelectorAll(".operator")
+const result = document.querySelector(".result")
+
+let notResult = true
+    
+// }
+let digits = [];
+let equation = []
+
+    for (const button of Object.values(calcButton)) {
+        button.addEventListener("click", () => {
+            console.log(button.value)
+        })
+        button.addEventListener("click", () => {screen.innerText += button.value})
+        button.addEventListener("click", () => {digits.push(button.value)})
+    }
+
+    for (const button of Object.values(calcOperator)) {
+        button.addEventListener("click", () => {
+            console.log(button.value)
+        })
+        button.addEventListener("click", () => {screen.innerText += button.value})
+        button.addEventListener("click", () => {
+            let userNumber = digits.join('');
+            console.log(userNumber)
+            equation.push(userNumber)
+            equation.push(button.value)
+            console.log(equation)
+        })
+    }
 
 
-for (const button of Object.values(calcButton)) {
-    button.addEventListener("click", () => {
-        console.log(button.value)
-    })
-}
-for (const button of Object.values(calcOperator)) {
-    button.addEventListener("click", () => {
-        console.log(button.value)
-    })
-}
+    // result.addEventListener("click", () => {
 
+    // })
+
+
+// function userPushEqual() {
+    
+// }
+
+// function addToScreen() {
+// screen.innerText = button.value
+// }
 
