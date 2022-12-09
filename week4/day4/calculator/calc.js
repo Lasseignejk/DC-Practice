73ef12ff11ee5ebcffff2aa49e7f5546ecbc7129
Lasseignejk <jaye914@hotmail.com>
2022-12-08 22:10:27 -0500

    updates to calculator
    Mostly done, might try to add a backspace button tomorrow, but other than that I'm happy

--- week4/day4/calculator/calc.js
+++ week4/day4/calculator/calc.js
@@ -1,63 +1,58 @@
 // select, create/modify, append
 
-
-
 // select
-const calcDiv = document.querySelector(".mainContainer");
+const calcDiv = document.querySelector(".calculator");
 const screen = document.querySelector(".numScreen")
+const userMathDiv = document.querySelector(".math")
+const answerDiv = document.querySelector(".answer")
 // this returns an object/dictionary
 const calcButton = document.querySelectorAll(".num")
 const calcOperator = document.querySelectorAll(".operator")
 const result = document.querySelector(".result")
-
-let notResult = true
+const clear = document.querySelector(".clear")
     
 // }
 let digits = [];
-let equation = []
+let equation = [];
+const space = " ";
 
-    for (const button of Object.values(calcButton)) {
-        button.addEventListener("click", () => {
-            console.log(button.value)
-        })
-        button.addEventListener("click", () => {screen.innerText += button.value})
-        button.addEventListener("click", () => {digits.push(button.value)})
-    }
+for (const button of Object.values(calcButton)) {
+    button.addEventListener("click", () => {
+        console.log(button.value);
+        userMathDiv.innerText += button.value;
+        digits.push(button.value)
+    });
+}
 
-    for (const button of Object.values(calcOperator)) {
-        button.addEventListener("click", () => {
-            console.log(button.value)
-        })
-        button.addEventListener("click", () => {screen.innerText += button.value})
-        button.addEventListener("click", () => {
-            let userNumber = digits.join('');
-            console.log(userNumber)
-            equation.push(userNumber)
-            equation.push(button.value)
-            console.log(equation)
-        })
-        button.addEventListener("click", () => {
-            digits.length = 0;
-        })
-    }
-
-    result.addEventListener("click", () => {
-        let lastUserNumber = digits.join('');
-        console.log(lastUserNumber)
-        equation.push(lastUserNumber)
-        let answer = equation.join('');
-        let realAnswer = eval?.(`"use strict";(${answer})`)
-        console.log(realAnswer)
-        screen.innerText = realAnswer
-    })
-
-
-
-// function userPushEqual() {
+for (const button of Object.values(calcOperator)) {
+    button.addEventListener("click", () => {
+        console.log(button.value)
+        userMathDiv.innerText += button.value;
+        userMathDiv.innerText += space;
+        let userNumber = digits.join('');
+        console.log(userNumber);
+        equation.push(userNumber);
+        equation.push(button.value);
+        console.log(equation);
+        digits.length = 0;
+        userMathDiv.innerText += space;
     
-// }
+    })
+}
 
-// function addToScreen() {
-// screen.innerText = button.value
-// }
+result.addEventListener("click", () => {
+    let lastUserNumber = digits.join('');
+    console.log(lastUserNumber)
+    equation.push(lastUserNumber)
+    let answer = equation.join('');
+    let realAnswer = eval?.(`"use strict";(${answer})`)
+    console.log(realAnswer)
+    answerDiv.innerText = realAnswer
+})
 
+clear.addEventListener("click", () => {
+    digits.length = 0;
+    equation.length = 0;
+    answerDiv.innerText = ""
+    userMathDiv.innerText = ""
+})
