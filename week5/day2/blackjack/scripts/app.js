7e7c0590be5bb62444ab236fe165ba4714766bc6
Lasseignejk <jaye914@hotmail.com>
2022-12-13 15:57:56 -0500

    Updates to blackjack
    Possibly done through step 10. Added random number generator to choose a random card, and I added splice to remove that card from the deck.

--- week5/day2/blackjack/scripts/app.js
+++ week5/day2/blackjack/scripts/app.js
@@ -2,12 +2,12 @@
 // 2 [x] Style the page
 // 3 [x] Cards on the table
 // 4 [x] Dealing the cards
-// 5 [] Hit me
-// 6 [] Creating a deck
-// 7 [] Deal the deck
-// 8 [] Image from cards
-// 9 [] Render hands 
-// 10 [] Shuffle the deck
+// 5 [x] Hit me
+// 6 [x] Creating a deck
+// 7 [x] Deal the deck
+// 8 [x] Image from cards
+// 9 [x] Render hands 
+// 10 [x] Shuffle the deck
 // 11 [] Calculate points for a hand 
 // 12 [] Display points
 // 13 [] Busts
@@ -18,26 +18,58 @@
 const dealerHand = document.getElementById("dealer-hand");
 const playerHand = document.getElementById("player-hand");
 const deck = [];
+const dealerCards = [];
+const playerCards = [];
 const suits = ["hearts", "spades", "clubs", "diamonds"];
-const ranks = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
-// const makeDeck = (rank, suit) => {
-//   const card = {
-//     rank: rank,
-//     suit: suit,
-//     pointValue: rank > 10 ? 10 : rank,
-//   };
-//   deck.push(card);
-// };
+const ranks = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
-// for (let suit of suits) {
-//   for (const rank of ranks) {
-//     makeDeck(rank, suit);
-//   }
-// }
+
+const makeDeck = (rank, suit) => {
+  const card = {
+    rank: rank,
+    suit: suit,
+    pointValue: rank > 10 ? 10 : rank,
+  };
+  deck.push(card);
+};
+for (let suit of suits) {
+  for (const rank of ranks) {
+    makeDeck(rank, suit);
+  }
+}
+
+function dealToPlayer() {
+  const randomNum = Math.floor(Math.random()*deck.length);
+  card = deck.splice(randomNum, 1)
+  console.log(deck)
+  // playerCards.push(card)
+  // getCardImage(playerCards)
+  playerHand.append(getCardImage(card))
+}
+
+function dealToDealer() {
+  const randomNum = Math.floor(Math.random()*deck.length);
+  card = deck.splice(randomNum, 1)
+  // dealerCards.push(card)
+  // getCardImage(dealerCards)
+  dealerHand.append(getCardImage(card))
+}
+
+const getCardImage = (array) => {
+  for (const item of array) {
+    // console.log(item)
+  let newCard = document.createElement("img");
+  newCard.setAttribute("src", `/week5/day2/blackjack/images/${item["rank"]}_of_${item["suit"]}.png`)
+
+ return newCard
+  }
+}
 
 const dealButton = document.getElementById("deal-button")
+const hitButton = document.getElementById("hit-button")
 
 dealButton.addEventListener("click", () => deal())
+hitButton.addEventListener("click", () => hit())
 
 function deal() {
   dealToPlayer();
@@ -46,17 +78,13 @@ function deal() {
   dealToDealer();
 }
 
-function dealToPlayer() {
-  const card = document.createElement("img");
-  card.setAttribute("src", "2_of_hearts.png");
-  playerHand.append(card);
+function hit() {
+  dealToPlayer();
+  dealToDealer();
 }
 
-function dealToDealer() {
-  const card = document.createElement("img");
-  card.setAttribute("src", "2_of_spades.png");
-  dealerHand.append(card);
-}
+
+
 
 
 
