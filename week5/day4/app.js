29d86871b39ce70a6a89b2dd03634ed5c68040f0
Lasseignejk <jaye914@hotmail.com>
2022-12-15 21:55:57 -0500

    Updates to weather app
    Final edits for tonight, put in grid to organize location boxes.

--- week5/day4/app.js
+++ week5/day4/app.js
@@ -16,31 +16,47 @@ const getWeather = async () => {
   console.log(json);
 
   const infoDiv = document.createElement("div");
+
+  const noneDiv = document.createElement("div");
+  noneDiv.classList.add("none")
+
   const deleteButton = document.createElement("button");
+  const deleteButtonDiv = document.createElement("div");
+
   const userLocation = document.createElement("h1");
+  const userLocationDiv = document.createElement("div");
+
   const userTemp = document.createElement("p");
+  const userTempDiv = document.createElement("div");
+
   const userFeelsLike = document.createElement("p");
+  const userFeelsLikeDiv = document.createElement("div");
+
   const userWeatherIcon = document.createElement("img");
+  const userWeatherIconDiv = document.createElement("div");
 
   let location = json.name;
   let temp = json.main.temp;
   let feelsLike = json.main.feels_like;
   let icon = json.weather[0]["icon"];
-  console.log(icon)
 
   infoDiv.classList.add("info");
 
   deleteButton.innerHTML = "X";
   deleteButton.classList.add("deleteButton")
   deleteButton.addEventListener("click", () => removeLocation(infoDiv))
-
-  deleteButton.classList.add("deleteButtonDiv")
+  deleteButtonDiv.append(deleteButton)
+  deleteButtonDiv.classList.add("delete")
 
   userLocation.innerHTML = location;
   userLocation.classList.add("location")
+  userLocationDiv.append(userLocation)
+  userLocationDiv.classList.add("header")
 
   userWeatherIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
   userWeatherIcon.classList.add("weatherPhoto")
+  userWeatherIconDiv.append(userWeatherIcon)
+  userWeatherIconDiv.classList.add("image")
 
   if (icon.search("d") >= 0) {
     infoDiv.classList.add("dayTime")
@@ -50,12 +66,17 @@ const getWeather = async () => {
     userWeatherIcon.classList.add("weatherPhotoNight")
   }
 
-  userTemp.innerHTML = `Currently: ${temp}&deg;F`
+  userTemp.innerHTML = `${temp}&deg;F`
+  userTempDiv.append(userTemp)
+  userTempDiv.classList.add("current")
 
-  userFeelsLike.innerHTML = `Feels like: ${feelsLike} &deg;F` 
+  userFeelsLike.innerHTML = `Feels like: ${feelsLike} &deg;F`
+  userFeelsLikeDiv.append(userFeelsLike) 
+  userFeelsLikeDiv.classList.add("feelsLike")
 
-  infoDiv.append(deleteButton, userLocation, userWeatherIcon, userTemp, userFeelsLike)
+  infoDiv.append(noneDiv, deleteButtonDiv, userLocationDiv, userWeatherIconDiv, userTempDiv, userFeelsLikeDiv)
   locationContainer.append(infoDiv)
+  zipInput.value = ""
 };
 
 function removeLocation(div) {
