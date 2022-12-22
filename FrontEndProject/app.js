584659f96148a1c1aeebc048064f98465235f375
Lasseignejk <jaye914@hotmail.com>
2022-12-22 17:04:43 -0500

    more updates to frontend project
    Images are working for each card, input field works, working on replacing it with a dropdown menu where user can select a state instead of typing

--- FrontEndProject/app.js
+++ FrontEndProject/app.js
@@ -1,7 +1,11 @@
 const cardContainer = document.querySelector(".cardContainer")
 
-const searchParkAPI = async () => {
 
+input = "SC"
+const searchParkAPI = async () => {
+    cardContainer.innerHTML="";
+    const searchField = document.querySelector(".inputField").value.toUpperCase();
+  
     // const response = await fetch(url, {
     //   method: "GET",
     //   parameters: {
@@ -10,6 +14,7 @@ const searchParkAPI = async () => {
     // });
 
     const response = await fetch(url)
+    console.log(url)
     const json = await response.json();
     const jsonData = json.data
     console.log(json);
@@ -18,22 +23,82 @@ const searchParkAPI = async () => {
         console.log(item)
         jsonName = item.fullName;
         jsonState = item.states;
-        jsonWeatherInfo = item.weatherInfo;
+        // jsonWeatherInfo = item.weatherInfo;
+        jsonZipCode = item.addresses[0]["postalCode"];
+        jsonCity = item.addresses[0]["city"];
+        jsonStreetAddress = item.addresses[0]["line1"];
+        jsonStateCode = item.addresses[0]["stateCode"]
+        jsonImage = item.images[0]["url"];
+        jsonDescription = item.description;
+        // jsonFee = parseInt(item.entranceFees[0]["cost"]);
+        // console.log(jsonFee) 
+        // const checkEntranceFee = (entranceFee) => {
+        //     if (entranceFee === 0) {
+        //         const jsonEntranceFee = "Free!"
+        //         return jsonEntranceFee
+        //     } else {
+        //         const jsonEntranceFee = '$' + jsonFee;
+        //         return jsonEntranceFee
+        //     }
+        // }
 
+        // ----------------------create-----------------------
         const card = document.createElement("div");
+        const front = document.createElement("div");
+        const back = document.createElement("div");
         const parkName = document.createElement("h1");
         const parkState = document.createElement("h2");
-        const parkWeatherInfo = document.createElement("p")
+        const parkAddress = document.createElement("p");
+        const parkDescription = document.createElement("p");
 
+        const checkDescriptionLength = (str) => {
+            if (str.length > 380) {
+                const slicedString = str.slice(0, 380);
+                const newString = slicedString + "..."
+                return newString
+            } else {
+                const newString = str;
+                return newString
+            }
+        }
+        // const parkActivities = document.createElement("span");
+        // const parkEntranceFees = document.createElement("p");
+        // const parkWeatherInfo = document.createElement("p");
+
+        // -----------------------modify------------------------
+        front.style.backgroundImage = `url(${jsonImage})`
         parkName.innerHTML = `${jsonName}`;
         parkState.innerHTML = `${jsonState}`;
-        parkWeatherInfo.innerHTML = `${jsonWeatherInfo}`;
+        parkAddress.innerHTML = `Address: ${jsonStreetAddress + ", " + jsonCity + ", " + jsonStateCode + " " + jsonZipCode}`
+        parkDescription.innerHTML = `${checkDescriptionLength(jsonDescription)}`
+        // parkEntranceFees.innerHTML = `Entrance Fee: ${checkEntranceFee(jsonFee)}`
+        // parkWeatherInfo.innerHTML = `${jsonWeatherInfo}`;
 
-        card.append(parkName, parkState, parkWeatherInfo)
+        // ------------------modify classes---------------------
+        card.classList.add("card")
+        front.classList.add("front")
+        back.classList.add("back")
+        parkName.classList.add("parkName")
+        parkState.classList.add("parkState")
+
+        // ----------------------append-----------------------
+        front.append(parkName, parkState)
+        back.append(parkAddress, parkDescription)
+        card.append(front, back)
         cardContainer.append(card)
     }
 
 }
-//   };
 
-searchParkAPI()
+// ------------------Event listeners--------------------
+const searchButton = document.querySelector(".searchButton")
+
+searchButton.addEventListener("click", searchParkAPI)
+
+
+const searchField = document.querySelector(".inputField")
+searchField.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        searchParkAPI()
+    }
+}) 
