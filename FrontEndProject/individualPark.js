0c4f1de141c73e36e8165d3a57f5df8195c91114
Lasseignejk <jaye914@hotmail.com>
2023-01-03 16:10:14 -0600

    responsive work on individualPark page
    Working on media queries; also refactored the checkAlert to be totally in JS instead of html and added a modal

--- FrontEndProject/individualPark.js
+++ FrontEndProject/individualPark.js
@@ -8,6 +8,13 @@ const phoneDiv = document.querySelector(".phoneDiv")
 const addressDiv = document.querySelector(".addressDiv")
 const parkDescription = document.querySelector(".parkDescription")
 const parkActivities = document.querySelector(".parkActivities")
+const alertContainer = document.querySelector(".alertContainer")
+const alertButton = document.querySelector(".alertButton")
+const modal = document.querySelector(".modal")
+const closeModal = document.querySelector(".closeModal")
+const modalText = document.querySelector(".modalText")
+
+let recentSearch = []
 
 const getIndividualPark = async () => {
     const userPark = window.localStorage.getItem('park')
@@ -21,6 +28,9 @@ const getIndividualPark = async () => {
     console.log(jsonData);
 
     const jsonName = jsonData.fullName;
+    recentSearch.push(jsonName)
+    window.localStorage.setItem('recentSearch', recentSearch)
+
     const jsonWeatherInfo = jsonData.weatherInfo;
     const jsonZipCode = jsonData.addresses[0]["postalCode"];
     console.log(jsonZipCode)
@@ -71,7 +81,60 @@ const getIndividualPark = async () => {
         const json = await response.json();
         const jsonData = json.data
         console.log(jsonData);
-    }
+
+        const checkAlert = (jsonData) => {
+            if (jsonData.length >= 1) {
+                // const alertContainer = document.createElement("div")
+                // alertContainer.classList.add("alertContainer")
+                const alertButton = document.createElement("button")
+                alertButton.classList.add("showAlerts")
+                alertButton.classList.add("alertButton")
+                alertButton.classList.add("openModal")
+                const modal = document.createElement("div")
+                modal.classList.add("modal")
+                const modalText = document.createElement("div")
+                modalText.classList.add("modalText")
+                const closeModal = document.createElement("button")
+                closeModal.classList.add("closeModal")
+                closeModal.innerHTML = "X"
+                modalText.append(closeModal)
+
+                for (let i = 0; i < jsonData.length; i++) {
+                    const alertTitle = document.createElement("h2");
+                    alertTitle.classList.add("alertTitle")
+                    alertTitle.innerHTML = `${jsonData[i]["title"]}`
+                    const alertDescription = document.createElement("p");
+                    alertDescription.classList.add("alertDescription")
+                    alertDescription.innerHTML = `${jsonData[i]["description"]}`
+                    modalText.append(alertTitle, alertDescription)
+                    modal.append(modalText)
+                    alertContainer.append(alertButton, modal)
+                    // parkInformationContainer.append(alertContainer)
+                } 
+                if (jsonData.length === 1) {
+                    alertButton.innerHTML = "There is 1 alert for this location."
+                } else if (jsonData.length > 1) {
+                    alertButton.innerHTML = `There are ${jsonData.length} alerts for this location.`  
+                }
+                alertButton.addEventListener("click", () => {
+                    modal.style.display = "block";
+                })
+                closeModal.addEventListener("click", () => {
+                    modal.style.display = "none";
+                })
+                window.addEventListener("click", (e) => {
+                    if (e.target == modal) {
+                        modal.style.display = "none";
+                    }
+                })
+             
+            } else {
+                console.log("There are no alerts for this location.")
+                alertContainer.remove()
+            }
+        }
+        checkAlert(jsonData)
+        }
 
     getParkAlerts(jsonParkCode)
 
@@ -126,6 +189,8 @@ heart.addEventListener("click", () => {
 solidHeart.addEventListener("click", () => {
     solidHeartSpan.classList.remove("showHeart")
 })
+
+
 
 
 getIndividualPark()
