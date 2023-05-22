62ee66048b5f902b882f03686990e5c3a959a80c
Lasseignejk <jaye914@hotmail.com>
2023-05-22 10:38:30 -0400

    removed ta work
    moved folders into another repo

--- TA/week3/DOM/eventListeners/review/script.js
+++
@@ -1,8 +0,0 @@
-console.log("connected");
-const button = document.querySelector("#button");
-
-button.addEventListener("click", function () {
-	console.log("Hello");
-});
-
-// button.addEventListener("click", () => console.log("Hello"));
